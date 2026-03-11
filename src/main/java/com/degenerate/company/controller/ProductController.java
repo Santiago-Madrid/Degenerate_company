d48c34cf77d23f38dc55df13564cb0386df962a9
Santiago-Madrid <jhoansanti1509@gmail.com>
2026-03-11 07:57:16 -0500

    feat: Creacion de producto con su stock

--- src/main/java/com/degenerate/company/controller/ProductController.java
+++ src/main/java/com/degenerate/company/controller/ProductController.java
@@ -1,5 +1,33 @@
 package com.degenerate.company.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.degenerate.company.dto.ProductRequestDTO;
+import com.degenerate.company.dto.ProductResponseDTO;
+import com.degenerate.company.service.LicorService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/products")
 public class ProductController {
     
+    private final LicorService licorService;
+
+    @PostMapping 
+    public ResponseEntity<ProductResponseDTO> createProduct(@RequestBody ProductRequestDTO productRequestDTO) {
+        try {
+            ProductResponseDTO response = licorService.createProduct(productRequestDTO);
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+    }
 }
